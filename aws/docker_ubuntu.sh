53f3b1b6b4edd18e451367a2275f290531dc7c77
Hardik Patel <hardikgw@gmail.com>
2018-03-17 16:10:06 -0400

    aws docker install script

---
+++ aws/docker_ubuntu.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common\
+    -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce -y
+sudo apt-get install docker-compose -y
+sudo groupadd docker
+sudo usermod -aG docker $USER
