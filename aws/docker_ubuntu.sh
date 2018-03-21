7b58ea9ca06db9034909f4fb387faf022c0371e5
Hardik Patel <hardikgw@gmail.com>
2018-03-21 16:45:34 -0400

    upgrades

--- aws/docker_ubuntu.sh
+++ aws/docker_ubuntu.sh
@@ -13,9 +13,11 @@ sudo add-apt-repository \
    stable"
 sudo apt-get update
 sudo apt-get install docker-ce -y
-sudo apt-get install docker-compose -y
+sudo curl -L https://github.com/docker/compose/releases/download/1.20.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo groupadd docker
 sudo usermod -aG docker $USER
+sudo sysctl -w vm.max_map_count=262144
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
 sudo apt-get install git-lfs
 git lfs install
